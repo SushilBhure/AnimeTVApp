7dc7d0e1b916afa38a2eef433c1a0efa6a30430e
sushil <bhure.sushil91@gmail.com>
2026-06-13 12:21:32 +0530

    test_commit

--- app/src/main/java/com/sushil/animetvapp/MainActivity.kt
+++ app/src/main/java/com/sushil/animetvapp/MainActivity.kt
@@ -33,15 +33,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hello world",
         modifier = modifier
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AnimeTVAppTheme {
-        Greeting("Android")
-    }
-}
